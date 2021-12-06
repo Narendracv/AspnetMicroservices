32c3396f9a56a08d14084a59333d65a1f02ab487
Narendra Chaudhari <narendracv@gmail.com>
2021-12-06 14:19:10 +0530

    Catelog-API LAyer

---
+++ src/Catalog/Catlog.Api/Data/CatlogContext.cs
@@ -0,0 +1,25 @@
+﻿using Catlog.Api.Entities;
+using Microsoft.Extensions.Configuration;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catlog.Api.Data
+{
+    public class CatlogContext : ICatlogContext
+    {
+        public CatlogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            //Create/Get Existing DB
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
+            //CatlogContextSeed.SeedData(Products);
+        }
+        public IMongoCollection<Product> Products { get; }
+    }
+    
+    
+}
---
+++ src/Catalog/Catlog.Api/Data/ICatlogContext.cs
@@ -0,0 +1,14 @@
+﻿using Catlog.Api.Entities;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Catlog.Api.Data
+{
+    public interface ICatlogContext
+    {
+        IMongoCollection<Product> Products { get; }
+    }
+}
